5aaf13ce9ecdc7e2fbb4b2f1af13789b7d8d1903
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2021-06-04 13:32:11 -0700

    remove default website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="text-red-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hi</h1>
+            <p>
+                Welcome to Nathan Adam's portfolio website.
+                <br /><br />
+                I still haven't come up with an idea for a design, so check back later. In the meantime, you can visit my projects website at <a href="https://schobbish.com">schobbish.com</a>.
+            </p>
+        </div>
+    );
 }
 
 export default App;

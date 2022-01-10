584572040ba32d85d111221284b8e8a608b8b6e7
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2022-01-09 20:51:22 -0800

    redo some css

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,10 +7,19 @@ export class Projects extends React.Component<SubpageContainerProps> {
   render() {
     return (
       <SubpageContainer overlay={this.props.overlay} onClickUnmountSubpageHandler={this.props.onClickUnmountSubpageHandler}>
-        Projects page
+        <h2 className="std-h2">My Projects</h2>
       </SubpageContainer>
     );
   }
 }
+
+
+class ProjectCard extends React.Component<ProjectCardProps> {
+
+}
+type ProjectCardProps = {
+
+}
+
 
 export default Projects;
